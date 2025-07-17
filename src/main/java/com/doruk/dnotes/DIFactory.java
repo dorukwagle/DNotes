6a3de4a0246d7762347c324a0c259b40cb224ed5
dorukwagle <dorukwagle@gmail.com>
2025-07-17 14:43:58 +0545

    add factory to create preferences objects

--- src/main/java/com/doruk/dnotes/DIFactory.java
+++ src/main/java/com/doruk/dnotes/DIFactory.java
@@ -1,5 +1,15 @@
 package com.doruk.dnotes;
 
+import com.doruk.dnotes.interfaces.IPreference;
+import com.doruk.dnotes.prefs.EditorPreference;
+import com.doruk.dnotes.prefs.GlobalPreference;
+
 public class DIFactory {
-    
+    public static IPreference createGlobalPreference() {
+        return GlobalPreference.getInstance();
+    }
+
+    public static IPreference createEditorPreference() {
+        return EditorPreference.getInstance();
+    }
 }
