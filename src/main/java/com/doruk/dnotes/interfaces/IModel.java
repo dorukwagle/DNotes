e4f3c6b948b2401c25da4a6e23141915356ffc2e
dorukwagle <dorukwagle@gmail.com>
2025-07-21 14:22:16 +0545

    change IModel interface to support sorting methods

--- src/main/java/com/doruk/dnotes/interfaces/IModel.java
+++ src/main/java/com/doruk/dnotes/interfaces/IModel.java
@@ -1,10 +1,14 @@
 package com.doruk.dnotes.interfaces;
 
 import java.util.List;
+import java.util.Optional;
+
+import com.doruk.dnotes.enums.SortBy;
+import com.doruk.dnotes.enums.SortOrder;
 
 public interface IModel<T> {
     T add(T t);
     T update(T t);
     void delete(String id);
-    List<T> getAll();
+    List<T> getAll(Optional<SortBy> sortBy, Optional<SortOrder> sortOrder);
 }
--- src/main/java/com/doruk/dnotes/models/CollectionModel.java
+++ src/main/java/com/doruk/dnotes/models/CollectionModel.java
@@ -1,8 +1,11 @@
 package com.doruk.dnotes.models;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.doruk.dnotes.dto.CollectionDto;
+import com.doruk.dnotes.enums.SortBy;
+import com.doruk.dnotes.enums.SortOrder;
 import com.doruk.dnotes.interfaces.IModel;
 
 public class CollectionModel implements IModel<CollectionDto> {
@@ -22,7 +25,7 @@ public class CollectionModel implements IModel<CollectionDto> {
     }
 
     @Override
-    public List<CollectionDto> getAll() {
+    public List<CollectionDto> getAll(Optional<SortBy> sortBy, Optional<SortOrder> sortOrder) {
         return null;
     }
 }
