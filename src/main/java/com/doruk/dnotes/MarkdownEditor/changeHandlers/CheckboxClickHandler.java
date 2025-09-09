21c2aaada0b493e83e3d1819120a38747b3ebe8e
dorukwagle <dorukwagle@gmail.com>
2025-09-09 19:51:46 +0545

    implement checkbox click handler

--- src/main/java/com/doruk/dnotes/MarkdownEditor/changeHandlers/CheckboxClickHandler.java
+++ src/main/java/com/doruk/dnotes/MarkdownEditor/changeHandlers/CheckboxClickHandler.java
@@ -2,12 +2,13 @@ package com.doruk.dnotes.MarkdownEditor.changeHandlers;
 
 import java.util.function.Consumer;
 
-import com.doruk.dnotes.MarkdownEditor.Factory;
 import com.doruk.dnotes.MarkdownEditor.ToolsMediator;
-import com.doruk.dnotes.MarkdownEditor.enums.ToolName;
+import com.doruk.dnotes.MarkdownEditor.dto.ParagraphListItemInfo;
+import com.doruk.dnotes.MarkdownEditor.enums.ParagraphType;
 import com.doruk.dnotes.MarkdownEditor.enums.ToolsEvent;
 import com.doruk.dnotes.MarkdownEditor.interfaces.FXTextEditor;
-import com.doruk.dnotes.MarkdownEditor.interfaces.ListStyleTool;
+import com.doruk.dnotes.MarkdownEditor.utils.ParagraphStyleHelper;
+
 
 public class CheckboxClickHandler implements Consumer<Object[]> {
     private FXTextEditor editor;
@@ -18,18 +19,14 @@ public class CheckboxClickHandler implements Consumer<Object[]> {
 
     @Override
     public void accept(Object[] args) {
-        int index = (int) args[0];
+        int parIndex = (int) args[0];
+        var area = editor.getArea();
         
-        var checkboxTool = Factory.createTool(ToolName.CheckList, null);
-        if (!(checkboxTool instanceof ListStyleTool tool))
-            return;
+        var curStyle = area.getParagraph(parIndex).getParagraphStyle();
+        var itemInfo = new ParagraphListItemInfo(curStyle, parIndex, curStyle.level, curStyle.lineCount, !curStyle.isItemChecked, 
+            ParagraphType.CHECK_LIST_ITEM);
         
-        // tool.applyWithUpdatedState(editor, new ParagraphListItemInfo(
-        //     index,
-        //     0,
-        //     0,
-        //     false,
-        //     null
-        // ));
+        var style = ParagraphStyleHelper.withListNode(itemInfo, curStyle.numberListId, true);
+        area.setParagraphStyle(parIndex, style);
     }
 }
