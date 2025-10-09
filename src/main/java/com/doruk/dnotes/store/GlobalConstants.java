41d2cbabd35a31bfb2b34aaa3047468cfef895ee
dorukwagle <dorukwagle@gmail.com>
2025-10-09 16:40:08 +0545

    create path utility methods

--- src/main/java/com/doruk/dnotes/store/GlobalConstants.java
+++ src/main/java/com/doruk/dnotes/store/GlobalConstants.java
@@ -3,7 +3,7 @@ package com.doruk.dnotes.store;
 import javafx.scene.paint.Color;
 
 public class GlobalConstants {
-    public static final String APP_NAME = "DNotes";
+    public static final String APP_NAME = "dNotes";
     public static final String APP_VERSION = "1.0.0";
     public static final int APP_VERSION_CODE = 100;
     public static final int DEFAULT_FONT_SIZE = 18;
@@ -16,4 +16,6 @@ public class GlobalConstants {
     public static final int DEFAULT_LIST_ITEM_INSET = 30;
     public static final int MAX_LIST_ITEM_OFFSET = 10;
     public static final int OBFUSCATION_SEED_LENGTH = 32;
+    public static final String APP_FORMAT = ".dnt";
+    public static final String PACKAGE_NAME = "com.doruk.dNotes";
 }
