30416c7e7d24d5e664455c6bcbc9f11499855d03
dorukwagle <dorukwagle@gmail.com>
2025-10-15 22:21:32 +0545

    add build script, config pom.xml for native build

---
+++ build_native.sh
@@ -0,0 +1,10 @@
+native-image -jar target/DNotes-1.0.0.jar \                                                               ─╯
+-H:ConfigurationFileDirectories=META-INF/native-image \
+-H:Name=DNotes \
+--no-fallback \
+--enable-all-security-services \
+--report-unsupported-elements-at-runtime \
+-H:+AddAllCharsets \
+-H:EnableURLProtocols=http,https \
+-H:IncludeResources='.*\.css|.*\.png|.*\.ttf|.*\.fxml|org/sqlite/native/Linux/x86_64/libsqlitejdbc.so|org/sqlite/native/Linux/x86_64/libsqlite3.so' \
+-cp $HOME/.m2/repository/org/xerial/sqlite-jdbc/3.50.2.0/sqlite-jdbc-3.50.2.0.jar
