737cd19ac26314e3e62006fcaaa20a9ad4a97316
dorukwagle <dorukwagle@gmail.com>
2025-10-18 21:48:55 +0545

    implement shared/quick notes navigation and view/edit

--- src/main/java/com/doruk/dnotes/controllers/BookController.java
+++ src/main/java/com/doruk/dnotes/controllers/BookController.java
@@ -1,41 +1,33 @@
 package com.doruk.dnotes.controllers;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import com.doruk.dnotes.ControllerFactory;
 import com.doruk.dnotes.DIFactory;
 import com.doruk.dnotes.dto.BookPageDto;
 import com.doruk.dnotes.dto.PaginationParams;
 import com.doruk.dnotes.dto.SearchControlsDto;
-import com.doruk.dnotes.enums.Preference;
-import com.doruk.dnotes.enums.SortBy;
-import com.doruk.dnotes.enums.SortOrder;
-import com.doruk.dnotes.enums.ViewPage;
-import com.doruk.dnotes.interfaces.IBookView;
-import com.doruk.dnotes.interfaces.IController;
-import com.doruk.dnotes.interfaces.IEditorController;
-import com.doruk.dnotes.interfaces.IModel;
-import com.doruk.dnotes.interfaces.INavigationController;
-import com.doruk.dnotes.interfaces.IPreference;
+import com.doruk.dnotes.enums.*;
+import com.doruk.dnotes.interfaces.*;
 import com.doruk.dnotes.store.BookStore;
-
 import com.doruk.dnotes.utils.PathUtils;
 import javafx.application.Platform;
 import javafx.scene.Parent;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BookController implements IController {
 
     private final IBookView view;
     private final INavigationController navigationController;
     private IEditorController editorController;
-    private IPreference preference;
-    private IModel<BookPageDto> noteModel;
+    private final IPreference preference;
+    private final IModel<BookPageDto> noteModel;
     private List<BookPageDto> notes;
     private PaginationParams noteParams = new PaginationParams();
     private BookPageDto currentEditingNote;
     private static boolean isStartup = true;
     private static boolean isSearchProgress = false;
+    private final NoteType noteType;
 
     private enum StateAction {
         Create,
@@ -43,6 +35,7 @@ public class BookController implements IController {
         Update
     }
 
+    // always normal note
     public BookController(IBookView view, INavigationController navigationController) {
         this.view = view;
         this.navigationController = navigationController;
@@ -50,15 +43,37 @@ public class BookController implements IController {
         this.preference = DIFactory.createGlobalPreference();
         this.noteModel = DIFactory.createNoteModel();
 
+        this.noteType = NoteType.NORMAL;
+
         init();
     }
 
+    // this can be either shared note or quick note
     public BookController(IBookView view, INavigationController navigationController, IModel<BookPageDto> noteModel) {
         this.view = view;
         this.navigationController = navigationController;
         this.noteModel = noteModel;
+        this.preference = DIFactory.createGlobalPreference();
+
+        // now read the BookStore
+        this.noteType = BookStore.getNoteType();
+        var addNew = BookStore.isAddNewPage();
 
         init();
+
+        // if it's shared, disable creation
+        if (this.noteType == NoteType.SHARED) {
+            this.view.getNewNoteButton().setVisible(false);
+            return;
+        }
+
+        if (!addNew)
+            return;
+
+        // add new quick note
+        var note = this.createNewNote();
+        if (note != null)
+            this.openNote(note);
     }
 
     private void init() {
@@ -68,8 +83,11 @@ public class BookController implements IController {
 
         // check if last opened note belongs to this book, and also isn't deleted
         // then only open the note if in preferences
-        if (!isStartup)
+        if (!isStartup || this.noteType != NoteType.NORMAL)
             return;
+
+        // if its app startup, i.e. first time the book controller is opened.
+        // try to resume from the last position as per preference
 
         var lastNoteId = preference.loadString(Preference.LastOpenedNoteId, "");
         var exists = !lastNoteId.isEmpty() &&
@@ -96,12 +114,12 @@ public class BookController implements IController {
 
     private void openBook() {
         var book = BookStore.getSelectedBook();
-        if (book.isEmpty() || book.get().getId().isEmpty()) {
+        if (this.noteType == NoteType.NORMAL && (book.isEmpty() || book.get().getId().isEmpty())) {
             this.view.setPlaceholder("Selected book doesn't exist!");
             return;
         }
 
-        this.notes = this.noteModel.ofParentId(book.get().getId())
+        this.notes = this.noteModel.ofParentId(this.noteType == NoteType.NORMAL ? book.get().getId() : null)
                 .getAll(this.noteParams);
 
         var feed = this.notes.isEmpty() ? "No pages found! Create one to get started..."
@@ -111,7 +129,10 @@ public class BookController implements IController {
 
         this.view.setSidebarItems(this.notes);
 
-        // save last opened book
+        // save last opened book if it's normal book
+        if (this.noteType != NoteType.NORMAL)
+            return;
+
         this.preference.saveString(Preference.LastOpenedBookId, book.get().getId());
     }
 
@@ -136,23 +157,24 @@ public class BookController implements IController {
         this.preference.saveString(Preference.LastOpenedNoteId, note.getId());
     }
 
-    private void createNewNote() {
+    private BookPageDto createNewNote() {
         var model = DIFactory.createPromptModal("New Note", "Create a new note", "Name:");
         var res = model.showAndWait();
 
-        if (!res.isPresent() || res.get().trim().isEmpty())
-            return;
+        if (res.isEmpty() || res.get().trim().isEmpty())
+            return null;
 
         // create a new note, with the fileId as content
         var note = this.noteModel.add(new BookPageDto(
                 "",
-                BookStore.getSelectedBook().get().getId(),
+                this.noteType == NoteType.NORMAL ? BookStore.getSelectedBook().get().getId() : null,
                 res.get(),
                 PathUtils.generateFileId(),
                 "")
             );
 
         this.updateSidebarState(note, StateAction.Create);
+        return note;
     }
 
     private void updateSidebarState(BookPageDto note, StateAction action) {
@@ -196,7 +218,7 @@ public class BookController implements IController {
     private void updateNote(BookPageDto note, String updatedName) {
         var updatedNote = this.noteModel.update(new BookPageDto(
                 note.getId(),
-                BookStore.getSelectedBook().get().getId(),
+                this.noteType == NoteType.NORMAL ? BookStore.getSelectedBook().get().getId() : null,
                 updatedName,
                 note.getContentId(),
                 "")
