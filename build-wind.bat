f0b5beba11a075f53299e1a469b053a8f361a839
dorukwagle <dorukwagle@gmail.com>
2025-10-31 20:30:21 +0545

    create scripts and adjust pom.xml to support win/lin builds

--- build-wind.bat
+++ build-wind.bat
@@ -6,19 +6,20 @@ setlocal enabledelayedexpansion
 :: -----------------------------
 set APP_NAME=dNotes
 set APP_VERSION=1.0.0
-set MAIN_CLASS=com.doruk.dnotes.Main
+set MAIN_CLASS=com.doruk.dnotes.App
 
 set JAR_FILE=target\dNotes-%APP_VERSION%-shaded.jar
-set IMAGE_DIR=target\dNotes-win\jre
+set IMAGE_DIR=target\runtime\jre
 set APP_DIR=target\dNotes-win
 set MSI_DIR=target\dist-win
-set ICON_FILE=src\main\resources\icon.png
+set ICON_FILE=src\main\resources\icon.ico
 
 :: -----------------------------
 :: Clean previous build
 :: -----------------------------
 if exist "%APP_DIR%" rmdir /s /q "%APP_DIR%"
 if exist "%MSI_DIR%" rmdir /s /q "%MSI_DIR%"
+if exist "%IMAGE_DIR%" rmdir /s /q "%IMAGE_DIR%"
 
 mkdir "%APP_DIR%"
 
@@ -28,7 +29,7 @@ mkdir "%APP_DIR%"
 jlink ^
   --module-path "%JAVA_HOME%\jmods" ^
   --add-modules java.base,java.desktop,java.logging,jdk.crypto.ec,java.sql,javafx.base,javafx.controls,javafx.graphics ^
-  --compress=2 ^
+  --compress=zip-9 ^
   --strip-debug ^
   --no-header-files ^
   --no-man-pages ^
@@ -37,16 +38,14 @@ jlink ^
   --output "%IMAGE_DIR%"
 
 :: Remove unused JVM folders (server/client)
-rmdir /s /q "%IMAGE_DIR%\lib\server"
-rmdir /s /q "%IMAGE_DIR%\lib\client"
-ren "%IMAGE_DIR%\lib\minimal" server
+rmdir /s /q "%IMAGE_DIR%\bin\server"
+ren "%IMAGE_DIR%\bin\client" server
 
 :: -----------------------------
 :: Copy app jar, scripts, icon
 :: -----------------------------
 copy "%JAR_FILE%" "%APP_DIR%\dNotes.jar"
-xcopy /s /y scripts "%APP_DIR%\scripts"
-copy "%ICON_FILE%" "%APP_DIR%\icon.png"
+copy "%ICON_FILE%" "%APP_DIR%\icon.ico"
 
 :: -----------------------------
 :: Make launcher executable (if using .bat)
@@ -72,6 +71,7 @@ jpackage ^
   --win-shortcut ^
   --win-menu ^
   --win-dir-chooser ^
+  --win-upgrade-uuid "e7a42d7f-5fcb-4b3e-a236-0df23e12c9d1" ^
   --java-options "-Dfile.encoding=UTF-8 -Xms64m -Xmx256m"
 
 echo.
